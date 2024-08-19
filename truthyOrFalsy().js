1d3035c0b51fa4bd8f3ae91cdf73c492d97f664d
maria chen <130259515+mariaww@users.noreply.github.com>
2024-08-19 12:08:53 +0200

    Create truthyOrFalsy().js

---
+++ truthyOrFalsy().js
@@ -0,0 +1,16 @@
+// truthyOrFalsy(), that takes in any value and returns true if that value is truthy and false if that value is falsy:
+
+const truthyOrFalsy= input =>
+  { return input ? true : false;
+}
+
+
+
+console.log(truthyOrFalsy(0)) // Should print false
+
+
+
+
+
+
+
